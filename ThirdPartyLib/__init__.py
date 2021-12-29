9cd7c18a5f9296a0bb477e825bf2d422f0d9badc
Evan He <evan128@michaels.com>
2021-12-29 01:35:51 -0600

    add MySQL demo

---
+++ ThirdPartyLib/__init__.py
@@ -0,0 +1,22 @@
+#!usr/bin/ python3
+# -*- coding:utf-8 -*-
+"""
+@Description: $END$
+@Author: Evan He
+@Time: 12/29/2021 1:35 AM
+@Website: hezhilv@126.com
+@project: PythonDemo
+@FileName: __init__.py.py
+@Copyright: ©2019-2021 michaels.com
+"""
+
+
+class __init__.py(object):
+    '''
+    @ClassName：__init__.py
+    @Description：$END$
+    '''
+
+
+if __name__ == '__main__':
+    ci = __init__.py()
