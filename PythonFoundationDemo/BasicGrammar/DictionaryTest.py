39e7ef4ce9084a5de081e886c51dc32ec4ec3bde
Evan He <evan128@michaels.com>
2022-02-09 03:23:33 -0600

    update some code

--- PythonFoundationDemo/BasicGrammar/DictionaryTest.py
+++ PythonFoundationDemo/BasicGrammar/DictionaryTest.py
@@ -1,6 +1,10 @@
 #!usr/bin/ python3
 # -*- coding:utf-8 -*-
 
+import pprint
+
+
+
 """
 @Description: 
 @Author: hzlvxln
@@ -60,8 +64,33 @@ class DictionaryTest(object):
             count[character] += 1
 
         print(count)        # result: {'I': 1, 't': 6, ' ': 13, 'w': 2, 'a': 4, 's': 3, 'b': 1, 'r': 5, 'i': 6, 'g': 2, 'h': 3, 'c': 3, 'o': 2, 'l': 3, 'd': 3, 'y': 1, 'n': 4, 'A': 1, 'p': 1, ',': 1, 'e': 5, 'k': 2, '.': 1}
-
-
+        # 实现漂亮打印：键排过序
+        pprint.pprint(count)
+        ''' result:
+        {' ': 13,
+          ',': 1,
+          '.': 1,
+          'A': 1,
+          'I': 1,
+          'a': 4,
+          'b': 1,
+          'c': 3,
+          'd': 3,
+          'e': 5,
+          'g': 2,
+          'h': 3,
+          'i': 6,
+          'k': 2,
+          'l': 3,
+          'n': 4,
+          'o': 2,
+          'p': 1,
+          'r': 5,
+          's': 3,
+          't': 6,
+          'w': 2,
+          'y': 1}
+        '''
 
 if __name__ == '__main__':
     dict_test = DictionaryTest()
