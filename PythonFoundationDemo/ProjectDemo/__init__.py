5b17381317dc8b5e9d1419802937e02a8e00e348
hzl <363323479@qq.com>
2022-02-12 21:34:24 +0800

    add some test demo

---
+++ PythonFoundationDemo/ProjectDemo/__init__.py
@@ -0,0 +1,15 @@
+#!usr/bin/ python3
+# -*- coding:utf-8 -*-
+
+"""
+@Description: $END$
+@Author: hzlvxln
+@Time: 2022/2/12 12:13
+@Website: hezhilv@126.com
+@project: PythonDemo
+@FileName: __init__.py.py
+@Copyright: ©2019-2022 家灏信息科技有限公司
+"""
+
+if __name__ == '__main__':
+    pass
