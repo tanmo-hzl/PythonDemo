c17931e347c66565bccf97bc2b87fd9febadafa3
hzl <363323479@qq.com>
2022-02-12 11:56:45 +0800

    add some test demo

--- PythonFoundationDemo/BasicGrammar/ThirdLib/PyperclipTest.py
+++ PythonFoundationDemo/BasicGrammar/ThirdLib/PyperclipTest.py
@@ -1,16 +1,28 @@
-'''
-pyperclip 模块有copy()和paste()函数，可以向计算机的剪贴板发送文本，或从
-它接收文本。将程序的输出发送到剪贴板，使它很容易粘贴到邮件、文字处理程序
-或其他软件中。pyperclip 模块不是Python 自带的。要安装它，请遵从附录A 中安
-装第三方模块的指南。安装pyperclip 模块后，在交互式环境中输入以下代码：
->>> import pyperclip
->>> pyperclip.copy('Hello world!')
->>> pyperclip.paste()
-'Hello world!'
-当然，如果你的程序之外的某个程序改变了剪贴板的内容，paste()函数就会返
-回它。例如，如果我将这句话复制到剪贴板，然后调用paste()，看起来就会像这样：
->>> pyperclip.paste()
-'For example, if I copied this sentence to the clipboard and then called
-paste(), it would look like this:'
+#!usr/bin/ python3
+# -*- coding:utf-8 -*-
 
-'''
+import pyperclip
+
+
+"""
+@Description: pyperclip模块有copy()和paste()函数，可以向计算机的剪切板发送文本，或从计算机的剪切板接受文本。
+@Author: hzlvxln
+@Time: 2022/2/12 11:19
+@Website: hezhilv@126.com
+@project: PythonDemo
+@FileName: PyperclipTest.py
+@Copyright: ©2019-2022 家灏信息科技有限公司
+"""
+
+class PyperclipTest(object):
+    def test_copy_and_paste(self):
+        # pyperclip.copy()方法将文本copy进剪切板。
+        pyperclip.copy("Hello world")
+        # pyperclip.paste()方法将会取到剪切板的内容。
+        paste = pyperclip.paste()
+        print(paste)
+
+
+if __name__ == '__main__':
+    pyperclipTest = PyperclipTest()
+    pyperclipTest.test_copy_and_paste()
