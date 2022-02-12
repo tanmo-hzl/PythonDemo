1269e22b8f2a2311c09afbe3c205ef7be9b3b1d0
hzl <363323479@qq.com>
2022-02-12 21:56:35 +0800

    add regular test demo

---
+++ PythonFoundationDemo/BasicGrammar/RegularTest.py
@@ -0,0 +1,23 @@
+#!usr/bin/ python3
+# -*- coding:utf-8 -*-
+
+"""
+@Description:
+@Author: hzlvxln
+@Time: 2022/2/12 21:55
+@Website: hezhilv@126.com
+@project: PythonDemo
+@FileName: RegularTest.py
+@Copyright: ©2019-2022 家灏信息科技有限公司
+"""
+
+
+class RegularTest(object):
+    '''
+    @ClassName：RegularTest
+    @Description：
+    '''
+
+
+if __name__ == '__main__':
+    ci = RegularTest()
