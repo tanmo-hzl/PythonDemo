ba383a3eebcc12869441ecffea131c843544b861
Evan He <evan128@michaels.com>
2022-03-19 05:00:20 -0500

    update some RegexTest.py code

--- PythonFoundationDemo/BasicGrammar/RegexTest.py
+++ PythonFoundationDemo/BasicGrammar/RegexTest.py
@@ -238,8 +238,16 @@ class RegexTest(object):
         print(agentResultString)    # result: A**** told C**** that E**** knew B**** was a double agent.
 
 
-
-
+    def test_manage_complex_regex(self):
+        phoneRegex = re.compile(r'((\d{3}|\(\d{3}\))?(\s|-|\.)?\d{3}(\s|-|\.)\d{4}(\s*(ext|x|ext.)\s*\d{2,5})?)')
+        phoneRegexComment = re.compile(r'''(
+                            (\d{3}|\(\d{3}\))?                  # area code
+                            (\s|-|\.)?                          # separator
+                            \d{3}                               # first 3 digits
+                            (\s|-|\.)                           # separator
+                            \d{4}                               # last 4 digits
+                            (\s*(ext|x|ext.)\s*\d{2,5})?        # extension
+                            )''', re.VERBOSE)                   
 
 
 
