e0d6a3c9e1ab2f1888aa7efa4a84d82f6beb4ca9
Evan He <evan128@michaels.com>
2023-10-24 10:50:52 +0800

    update some code

--- LeetCode/leetcode/editor/cn/[1]两数之和.py
+++ LeetCode/leetcode/editor/cn/[1]两数之和.py
@@ -47,12 +47,44 @@
 
 
 # leetcode submit region begin(Prohibit modification and deletion)
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        dd
-        """
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: List[int]
+#         dd
+#         """
+from typing import List
+
+
+class Solution:
+    # 方法一：直接嵌套循环
+    def two_sum_1(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+        return []
+
+    # 方法二：通过hash表来进行匹配
+    def two_sum_2(self, nums: List[int], target: int) -> List[int]:
+        hashtable = dict()
+        for i, num in enumerate(nums):
+            if target - num in hashtable:       # 判断"target-num"的数是否在hashtable中。如果在，直接返回当前数的下标和 “target-num”的下标
+                return [hashtable[target - num], i]
+            hashtable[nums[i]] = i      # 在hashtable中没有找到"target-num"这个数，则将{num : i } 存入hashtable中，供下次查找使用
+        return []
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    nums = [3, 2, 4, 3]
+    target = 6
+    result = sol.two_sum_1(nums, target)
+    print(result)
+
+
 # leetcode submit region end(Prohibit modification and deletion)
